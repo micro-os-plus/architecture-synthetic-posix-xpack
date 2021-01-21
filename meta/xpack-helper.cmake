b474ed60a96e0eaaada9933b157130a9429eb558
Liviu Ionescu <ilg@livius.net>
2021-01-21 21:24:02 +0200

    xpack-helper.cmake: update

--- meta/xpack-helper.cmake
+++ meta/xpack-helper.cmake
@@ -11,28 +11,34 @@
 
 message(STATUS "Including micro-os-plus-architecture-synthetic-posix...")
 
-function(target_sources_micro_os_plus_architecture_synthetic_posix target)
+# -----------------------------------------------------------------------------
 
-  get_filename_component(PARENT_DIR ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
+function(target_sources_micro_os_plus_architecture target)
+
+  get_filename_component(xpack_root_folder ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
 
   target_sources(
     ${target}
 
     PRIVATE
-      ${PARENT_DIR}/src/lists.cpp
+      ${xpack_root_folder}/src/lists.cpp
   )
+
 endfunction()
 
-function(target_include_directories_micro_os_plus_architecture_synthetic_posix target)
+# -----------------------------------------------------------------------------
 
-  get_filename_component(PARENT_DIR ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
+function(target_include_directories_micro_os_plus_architecture target)
+
+  get_filename_component(xpack_root_folder ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
 
   target_include_directories(
     ${target}
 
     PUBLIC
-      ${PARENT_DIR}/include
+      ${xpack_root_folder}/include
   )
+
 endfunction()
 
 # -----------------------------------------------------------------------------
