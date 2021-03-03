104e40ed6f7d5ecb1bc50b78ee2837876eab6395
Liviu Ionescu <ilg@livius.net>
2021-03-04 00:30:10 +0200

    cmake: add xpack_process_package_version()

--- meta/micro-os-plus-architecture-synthetic-posix-config.cmake
+++ meta/micro-os-plus-architecture-synthetic-posix-config.cmake
@@ -18,7 +18,7 @@ endif()
 
 set(micro-os-plus-architecture-synthetic-posix-included TRUE)
 
-message(STATUS "Including micro-os-plus-architecture-synthetic-posix...")
+message(STATUS "Processing xPack ${PACKAGE_JSON_NAME}@${PACKAGE_JSON_VERSION}...")
 
 # -----------------------------------------------------------------------------
 # The current folder.
