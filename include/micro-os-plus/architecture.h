a2e8490a6015d0fbe265b0ed1c91737ba6594ad4
Liviu Ionescu <ilg@livius.net>
2026-08-04 22:18:32 +0300

    re-format includes

--- include/micro-os-plus/architecture.h
+++ include/micro-os-plus/architecture.h
@@ -18,7 +18,7 @@
 #if !(__cplusplus >= 202002L || (defined(_MSVC_LANG) && _MSVC_LANG >= 202002L))
 #error "C++20 or higher is required"
 #endif // !(__cplusplus >= 202002L || (defined(_MSVC_LANG) && _MSVC_LANG >=
-       // 202002L))
+// 202002L))
 #endif // defined(__cplusplus)
 
 #if __has_include("micro-os-plus/project-config.h")
